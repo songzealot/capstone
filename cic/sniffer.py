3c6ae687ea0b7d8ab0726e9d3e9eee72d671a4d5
songzealot <gjggjggjg123@naver.com>
2022-09-15 23:27:30 +0900

    주석 처리

--- cic/sniffer.py
+++ cic/sniffer.py
@@ -31,69 +31,69 @@ def create_sniffer(input_file, input_interface, output_mode, url_model=None):
         )
 
 
-def main():
-    parser = argparse.ArgumentParser()
+# def main():
+#     parser = argparse.ArgumentParser()
 
-    input_group = parser.add_mutually_exclusive_group(required=True)
-    input_group.add_argument(
-        "-i",
-        "--interface",
-        action="store",
-        dest="input_interface",
-        help="capture online data from INPUT_INTERFACE",
-    )
+#     input_group = parser.add_mutually_exclusive_group(required=True)
+#     input_group.add_argument(
+#         "-i",
+#         "--interface",
+#         action="store",
+#         dest="input_interface",
+#         help="capture online data from INPUT_INTERFACE",
+#     )
 
-    input_group.add_argument(
-        "-f",
-        "--file",
-        action="store",
-        dest="input_file",
-        help="capture offline data from INPUT_FILE",
-    )
+#     input_group.add_argument(
+#         "-f",
+#         "--file",
+#         action="store",
+#         dest="input_file",
+#         help="capture offline data from INPUT_FILE",
+#     )
 
-    output_group = parser.add_mutually_exclusive_group(required=False)
-    output_group.add_argument(
-        "-c",
-        "--csv",
-        "--flow",
-        action="store_const",
-        const="flow",
-        dest="output_mode",
-        help="output flows as csv",
-    )
+#     output_group = parser.add_mutually_exclusive_group(required=False)
+#     output_group.add_argument(
+#         "-c",
+#         "--csv",
+#         "--flow",
+#         action="store_const",
+#         const="flow",
+#         dest="output_mode",
+#         help="output flows as csv",
+#     )
 
-    url_model = parser.add_mutually_exclusive_group(required=False)
-    url_model.add_argument(
-        "-u",
-        "--url",
-        action="store",
-        dest="url_model",
-        help="URL endpoint for send to Machine Learning Model. e.g http://0.0.0.0:80/prediction",
-    )
+#     url_model = parser.add_mutually_exclusive_group(required=False)
+#     url_model.add_argument(
+#         "-u",
+#         "--url",
+#         action="store",
+#         dest="url_model",
+#         help="URL endpoint for send to Machine Learning Model. e.g http://0.0.0.0:80/prediction",
+#     )
 
-    parser.add_argument(
-        "output",
-        help="output file name (in flow mode) or directory (in sequence mode)",
-    )
+#     parser.add_argument(
+#         "output",
+#         help="output file name (in flow mode) or directory (in sequence mode)",
+#     )
 
-    args = parser.parse_args()
+#     args = parser.parse_args()
 
-    sniffer = create_sniffer(
-        args.input_file,
-        args.input_interface,
-        args.output_mode,
-        args.output,
-        args.url_model,
-    )
-    sniffer.start()
+#     sniffer = create_sniffer(
+#         args.input_file,
+#         args.input_interface,
+#         args.output_mode,
+#         args.output,
+#         args.url_model,
+#     )
+#     sniffer.start()
 
-    try:
-        sniffer.join()
-    except KeyboardInterrupt:
-        sniffer.stop()
-    finally:
-        sniffer.join()
+#     try:
+#         sniffer.join()
+#     except KeyboardInterrupt:
+#         sniffer.stop()
+#     finally:
+#         sniffer.join()
 
 
-if __name__ == "__main__":
-    main()
+# if __name__ == "__main__":
+#     main()
