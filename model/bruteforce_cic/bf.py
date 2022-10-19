1928d79d306c329cf4054c0945112fb13e1105d9
songzealot <gjggjggjg123@naver.com>
2022-10-20 01:00:06 +0900

    데이터 입력 수정

--- model/bruteforce_cic/bf.py
+++ model/bruteforce_cic/bf.py
@@ -12,15 +12,19 @@ detection_standard = 0.5
 
 def bruteForce(data):
     # print(f"리스트: {data}")
-    data = list(map(float, data))
+    # for i in data:
+    #     print(type(i))
+    # data = list(map(float, data))
     # print(f"플로트: {data}")
     data = np.array(data)
-    # print(data)
+    print(data)
     data = np.reshape(data, (1, 1, 21))
-    # print(data)
+    print(data)
     model_result = model.predict(data)
     model_result = model_result.tolist()
     model_result = model_result[0][0]
+
+    print(model_result)
 
     if (detection_standard < model_result) and (model_result <= 1):
         detection_result = 1
